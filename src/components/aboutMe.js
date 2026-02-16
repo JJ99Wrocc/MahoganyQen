09dee53a92f4b7525a79ee77ec2a2b1ea67a67df
Iwona Esangbedo <esangbedojoachim@gmail.com>
2026-02-17 00:35:07 +0100

    DOdanie ożywienia zdjęcia AboutMe

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -11,19 +11,19 @@ function AboutMe() {
     i18n.changeLanguage(lang);
   };
 
-return (
-  <div id="about-me" className="about-me-content">
-      {/* 1. WARSTWA ZDJĘCIA (tylko tło, ono będzie pływać) */}
-      <div className="about-me-img"></div>
-
-      {/* 2. WARSTWA TEKSTU (ona stoi sztywno na wierzchu) */}
-      <div className="about-me-text-layer">
-          <h2 className="about-me-h2">{t("aboutMeTitle")}</h2>
-          <hr className="about-me-hr" />
-          <p className="about-me-text">{t("aboutMeText")}</p>
+  return (
+    <div id="about-me" className="about-me-content">
+      <div className="about-me-row">
+        <div className="about-me-img">
+          <div className="about-me-p">
+            <h2 className="about-me-h2">{t("aboutMeTitle")}</h2>
+            <hr className="about-me-hr" />
+            <p className="about-me-text">{t("aboutMeText")}</p>
+          </div>
+        </div>
       </div>
-  </div>
-);
+    </div>
+  );
 }
 
 export default AboutMe;
