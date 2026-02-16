f21b22332018d0f94d75f57d2580ea8e04b869ba
Iwona Esangbedo <esangbedojoachim@gmail.com>
2026-02-16 23:53:10 +0100

    DOdanie ożywienia zdjęcia AboutMe

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,19 +7,17 @@ import i18n from '../i18n.js';
 function AboutMe() {
   const { t } = useTranslation();
 
-  const handleLanguageChange = (lang) => {
-    i18n.changeLanguage(lang);
-  };
-
   return (
     <div id="about-me" className="about-me-content">
       <div className="about-me-row">
-        <div className="about-me-img">
-          <div className="about-me-p">
-            <h2 className="about-me-h2">{t("aboutMeTitle")}</h2>
-            <hr className="about-me-hr" />
-            <p className="about-me-text">{t("aboutMeText")}</p>
-          </div>
+        {/* Zdjęcie jako osobny div (tylko tło) */}
+        <div className="about-me-img"></div>
+        
+        {/* Kontener na tekst (na wierzchu, nieruchomy) */}
+        <div className="about-me-text-container">
+          <h2 className="about-me-h2">{t("aboutMeTitle")}</h2>
+          <hr className="about-me-hr" />
+          <p className="about-me-text-p">{t("aboutMeText")}</p>
         </div>
       </div>
     </div>
